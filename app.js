15469e77a80089cc0ea81409ce386e8f731fef6b
Jose <josht_87@hotmail.com>
2020-06-23 22:17:01 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const sumar = (x,y) =>{
+    return x+y;
+}
+const restar = (x,y) =>{
+    return x-y;
+}
+const multiplicar = (x,y) =>{
+    return x*y;
+}
+const dividir = (x,y) =>{
+    return x/y;
+}
